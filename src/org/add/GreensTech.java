e879793157222aa61115cafddc2666d847945060
jaisri25 <jaisri4101996@gmail.com>
2022-08-20 11:14:00 +0530

    lll

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,18 @@
+package org.add;
+
+public class GreensTech {
+	
+	private void greensOmr(int a) {
+System.out.println("order is "+a);
+	}
+	
+	private void greensOmr(String a,int b) {
+System.out.println("type is "+a+"\n"+"number is "+b);
+	}
+	public static void main(String[] args) {
+		GreensTech g=new GreensTech();
+		g.greensOmr(25);
+		g.greensOmr("Software", 259);
+	}
+
+}
